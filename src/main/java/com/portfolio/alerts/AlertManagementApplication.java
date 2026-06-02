a67a05c5637641607300d47ee549303ed51fcf5d
SaiRohithT <137908985+SaiRohithT@users.noreply.github.com>
2026-06-02 01:41:07 -0500

    Add main application starter class

---
+++ src/main/java/com/portfolio/alerts/AlertManagementApplication.java
@@ -0,0 +1,11 @@
+package com.portfolio.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlertManagementApplication.class, args);
+    }
+}
